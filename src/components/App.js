ef0f69ef868e7adae46089979d765f47d005be36
Vanshika Verma <105100350+vanshikaverma13@users.noreply.github.com>
2024-05-19 10:50:36 +0530

    final working

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,50 @@
 import Editor from "./Editor";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import useLocalSrorage from "../hooks/useLocalSrorage";
 function App() {
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJs] = useState('')
+  const [html, setHtml] = useLocalSrorage('html', '')
+  const [css, setCss] = useLocalSrorage('css', '')
+  const [js, setJs] = useLocalSrorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      < html > 
+        <body>${html}</body> 
+        <style>${css}</style> 
+        <script>${js}</script>
+      </html >
+         `)
+    }, 250)
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+
+
   return (
     <>
       <div className="pane top-panel">
         <Editor
           language="xml"
           displayName="HTML"
-          value={ html}
+          value={html}
           onChange={setHtml} />
         <Editor
           language="css"
           displayName="CSS"
-          value={ css}
+          value={css}
           onChange={setCss} />
         <Editor
           language="jacascript"
           displayName="JS"
-          value={ js}
+          value={js}
           onChange={setJs} />
       </div>
       <div className="pane">
-        <iframe title="output"
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
           sandbox="allow-scripts"
           frameBorder="0"
           width="100%"
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+import {FontAwesomeIcon} from  '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt} from '@fortawesome/free-solid-svg-icons'
 
 export default function Editor(props) {
     const {
-        language, displayName, value,onChange
+        language,
+        displayName,
+        value,
+        onChange
     } = props
-    function handleChange(editor,data,value){
+    const [open, setOpen] = useState(true)
+    function handleChange(editor, data, value) {
         onChange(value)
     }
     return (
-        <div className="editor-container">
+        <div className={`editor-container ${open?'':'collapsed' }`}>
             <div className="editor-title">
                 {displayName}
-                <button>O/C</button>
+                <button 
+                type="button"
+                className="expand-collapse-btn"
+                onClick={()=>setOpen(prevOpen=>!prevOpen)}
+                > <FontAwesomeIcon icon={open? faCompressAlt : faExpandAlt}/> </button>
             </div>
             <ControlledEditor
                 onBeforeChange={handleChange}
@@ -31,7 +41,7 @@ export default function Editor(props) {
                     theme:'material'
                 }}
             />
-        </div>
+        </div >
     )
 
 }
